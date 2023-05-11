86e5a1a4a6f0030b6d0ef4a484c44cc749762565
Nathan SAEZ <115019735+SAEZN@users.noreply.github.com>
2023-05-11 13:08:07 +0200

    Add files via upload

---
+++ ProjetIntranet/index.php
@@ -0,0 +1,18 @@
+<?php require('include/entete.inc.php');?>
+<main>
+<div class="container w-50 h-50 mt-4" >
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/29714234-28635627.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/mener-reunion-equipe-efficace.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/istockphoto-696570136-612x612.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+<?php require('include/pied.inc.php');?>
+</main>
---
+++ ProjetIntranet/login.php
@@ -0,0 +1,67 @@
+<?php
+   session_start();
+   @$login=$_POST["login"];
+   @$pass=md5($_POST["pass"]);
+   @$valider=$_POST["valider"];
+   $erreur="";
+   if(isset($valider)){
+      include("connexion.php");
+      $sel=$pdo->prepare("select * from utilisateurs where login=? and pass=? limit 1");
+      $sel->execute(array($login,$pass));
+      $tab=$sel->fetchAll();
+      if(count($tab)>0){
+         $_SESSION["prenomNom"]=ucfirst(strtolower($tab[0]["prenom"])).
+         " ".strtoupper($tab[0]["nom"]);
+         $_SESSION["autoriser"]="oui";
+         header("location:index.php");
+      }
+      else
+         $erreur="Mauvais login ou mot de passe!";
+   }
+  ?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      <style>
+         *{
+            font-family:arial;
+         }
+         body{
+            margin:20px;
+         }
+         input{
+            border:solid 1px #2222AA;
+            margin-bottom:10px;
+            padding:16px;
+            outline:none;
+            border-radius:6px;
+         }
+         .erreur{
+            color:#CC0000;
+            margin-bottom:10px;
+         }
+         a{
+            font-size:12pt;
+            color:#EE6600;
+            text-decoration:none;
+            font-weight:normal;
+         }
+         a:hover{
+            text-decoration:underline;
+         }
+      </style>
+   </head>
+   <body onLoad="document.fo.login.focus()">
+      <h3> Authentification</h3>
+      <div class="erreur"><?php echo $erreur ?></div>
+      <form name="fo" method="post" action="">
+         <input type="text" name="login" placeholder="Login" /><br />
+         <input type="password" name="pass" placeholder="Mot de passe" /><br />
+         <input type="submit" name="valider" value="S'authentifier" />
+         <a href="inscription.php"> <button type="button" class="btn btn-secondary btn-lg">S'inscrire</button> </a>
+      </form>
+   </body>
+</html>
