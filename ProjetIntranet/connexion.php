5183136e5a71740dbf54e18968c1a8c6b60ef77e
Nathan SAEZ <115019735+SAEZN@users.noreply.github.com>
2024-05-17 17:25:31 +0200

    Delete ProjetIntranet directory

--- ProjetIntranet/connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-   try{
-      $pdo=new PDO("mysql:host=localhost;dbname=intranet","root","");
-   }
-   catch(PDOException $e){
-      echo $e->getMessage();
-   }
-?>
--- ProjetIntranet/suppr.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// Supprimez le fichier
-unlink('uploads/' . $_GET['file']);
-
-// Redirigez l'utilisateur vers la page d'accueil
- header('Location: PartageDocument.php?success=delete');
-exit();
-?>
